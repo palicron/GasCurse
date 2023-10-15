49af8c1e4344debbcc6d03c03a24852edbd7915c
Palicron <elgranpalicron@gmail.com>
2023-10-15 18:38:37 -0500

    class 10 Player controller

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -3,3 +3,33 @@
 
 #include "Player/AuraPlayerController.h"
 
+#include "EnhancedInputSubsystems.h"
+#include "InputMappingContext.h"
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	check(IsValid(AuraContext));
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(
+		GetLocalPlayer());
+
+	check(IsValid(Subsystem));
+
+	Subsystem->AddMappingContext(AuraContext, 0);
+
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	FInputModeGameAndUI InputModeDate;
+
+	InputModeDate.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeDate.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeDate);
+}
