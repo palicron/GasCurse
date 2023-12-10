652379674e08c9e2c667fdcd802a988c568ee688
Antares <elgranpalicron@gmail.com>
2023-12-10 12:06:41 -0500

    class 70

--- Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -59,24 +59,12 @@ public:
 	ATTRIBUTE_ACCESSORS(UAuraAttributeSet,Health)
 	UFUNCTION()
 	virtual void OnRep_Health(const FGameplayAttributeData& OldHealth);
-
-	UPROPERTY(ReplicatedUsing = OnRep_MaxHealth, EditAnywhere,BlueprintReadWrite,Category="Attribute Health")
-	FGameplayAttributeData MaxHealth;
-	ATTRIBUTE_ACCESSORS(UAuraAttributeSet,MaxHealth)
-	UFUNCTION()
-	virtual void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth);
-
+	
 	UPROPERTY(ReplicatedUsing = OnRep_Mana, EditAnywhere,BlueprintReadWrite,Category="Attribute Mana")
 	FGameplayAttributeData Mana;
 	ATTRIBUTE_ACCESSORS(UAuraAttributeSet,Mana)
 	UFUNCTION()
 	virtual void OnRep_Mana(const FGameplayAttributeData& OldMana);
-
-	UPROPERTY(ReplicatedUsing = OnRep_MaxMana, EditAnywhere,BlueprintReadWrite,Category="Attribute Mana")
-	FGameplayAttributeData MaxMana;
-	ATTRIBUTE_ACCESSORS(UAuraAttributeSet,MaxMana)
-	UFUNCTION()
-	virtual void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana);
 
 	/*Primary Attributes**/
 
@@ -103,7 +91,72 @@ public:
 	ATTRIBUTE_ACCESSORS(UAuraAttributeSet,Vigor)
 	UFUNCTION()
 	virtual void OnRep_Vigor(const FGameplayAttributeData& OldVigor);
+
+	/*Secondary Attributes*/
+
+	UPROPERTY(ReplicatedUsing = OnRep_MaxHealth, EditAnywhere,BlueprintReadWrite,Category="Attribute Health")
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet,MaxHealth)
+	UFUNCTION()
+	virtual void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth);
+
 	
+	UPROPERTY(ReplicatedUsing = OnRep_MaxMana, EditAnywhere,BlueprintReadWrite,Category="Attribute Mana")
+	FGameplayAttributeData MaxMana;
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet,MaxMana)
+	UFUNCTION()
+	virtual void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana);
+
+	UPROPERTY(ReplicatedUsing = OnRep_Armor, EditAnywhere,BlueprintReadWrite,Category="Secondary")
+	FGameplayAttributeData Armor;
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet,Armor)
+	UFUNCTION()
+	virtual void OnRep_Armor(const FGameplayAttributeData& OldArmor);
+
+	UPROPERTY(ReplicatedUsing = OnRep_ArmorPenetration, EditAnywhere,BlueprintReadWrite,Category="Secondary")
+	FGameplayAttributeData ArmorPenetration;
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet,ArmorPenetration)
+	UFUNCTION()
+	virtual void OnRep_ArmorPenetration(const FGameplayAttributeData& OldArmorPenetration);
+
+	UPROPERTY(ReplicatedUsing = OnRep_BlockChance, EditAnywhere,BlueprintReadWrite,Category="Secondary")
+	FGameplayAttributeData BlockChance;
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet,BlockChance)
+	UFUNCTION()
+	virtual void OnRep_BlockChance(const FGameplayAttributeData& OldBlockChance);
+
+	UPROPERTY(ReplicatedUsing = OnRep_CriticalHitChange, EditAnywhere,BlueprintReadWrite,Category="Secondary")
+	FGameplayAttributeData CriticalHitChange;
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet,CriticalHitChange)
+	UFUNCTION()
+	virtual void OnRep_CriticalHitChange(const FGameplayAttributeData& OldCriticalHitChange);
+
+	UPROPERTY(ReplicatedUsing = OnRep_CriticalHitDamage, EditAnywhere,BlueprintReadWrite,Category="Secondary")
+	FGameplayAttributeData CriticalHitDamage;
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet,CriticalHitDamage)
+	UFUNCTION()
+	virtual void OnRep_CriticalHitDamage(const FGameplayAttributeData& OldCriticalHitDamage);
+
+	UPROPERTY(ReplicatedUsing = OnRep_CriticalHitResistance, EditAnywhere,BlueprintReadWrite,Category="Secondary")
+	FGameplayAttributeData CriticalHitResistance;
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet,CriticalHitResistance)
+	UFUNCTION()
+	virtual void OnRep_CriticalHitResistance(const FGameplayAttributeData& OldCriticalHitResistance);
+
+	UPROPERTY(ReplicatedUsing = OnRep_HealthRegeneration, EditAnywhere,BlueprintReadWrite,Category="Secondary")
+	FGameplayAttributeData HealthRegeneration;
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet,HealthRegeneration)
+	UFUNCTION()
+	virtual void OnRep_HealthRegeneration(const FGameplayAttributeData& OldHealthRegeneration);
+
+	UPROPERTY(ReplicatedUsing = OnRep_ManaRegeneration, EditAnywhere,BlueprintReadWrite,Category="Secondary")
+	FGameplayAttributeData ManaRegeneration;
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet,ManaRegeneration)
+	UFUNCTION()
+	virtual void OnRep_ManaRegeneration(const FGameplayAttributeData& OldManaRegeneration);
+
+	
+		
 //----------------------------------------------------------------------------------------------------------------------
 	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
 
