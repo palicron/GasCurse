407332e838f7b0201029269b208889c50290ccec
Antares <elgranpalicron@gmail.com>
2024-01-24 23:01:39 -0500

    class 106

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -7,6 +7,8 @@
 #include "AuraGamePlayTags.h"
 #include "EnhancedInputSubsystems.h"
 #include "InputMappingContext.h"
+#include "NavigationPath.h"
+#include "NavigationSystem.h"
 #include "AbilitySystem/AuraAbilitySystemComponent.h"
 #include "Components/SplineComponent.h"
 #include "Input/AuraInputComponent.h"
@@ -122,10 +124,43 @@ void AAuraPlayerController::AbilityInputTagPressed(FGameplayTag InputTag)
 
 void AAuraPlayerController::AbilityInputTagReleased(FGameplayTag InputTag)
 {
-	if (GetASC())
+	if (!InputTag.MatchesTagExact(FAuraGamePlayTags::Get().InputTag_LMB))
 	{
-		GetASC()->AbilityInputTagReleased(InputTag);
+		if (GetASC())
+		{
+			GetASC()->AbilityInputTagReleased(InputTag);
+		}
+		return;
 	}
+
+	if(bTargeting)
+	{
+		if (GetASC())
+		{
+			GetASC()->AbilityInputTagReleased(InputTag);
+		}
+	}
+	else
+	{
+		const APawn* ControlledPawn = GetPawn();
+		if (FollowTime <= ShortPressThreshold && ControlledPawn)
+		{
+			if (UNavigationPath* NavPath = UNavigationSystemV1::FindPathToLocationSynchronously(
+				this, ControlledPawn->GetActorLocation(), CachedDestination))
+			{
+				Spline->ClearSplinePoints();
+				for (const FVector& pointLoc : NavPath->PathPoints)
+				{
+					Spline->AddSplinePoint(pointLoc,ESplineCoordinateSpace::World);
+					DrawDebugSphere(GetWorld(),pointLoc,8.f,8,FColor::Green,false,5.f);
+				}
+				bAutoRunning = true;
+			}
+		}
+		FollowTime = 0.f;
+		bTargeting = false;
+	}
+
 }
 
 void AAuraPlayerController::AbilityInputTagHeld(FGameplayTag InputTag)
