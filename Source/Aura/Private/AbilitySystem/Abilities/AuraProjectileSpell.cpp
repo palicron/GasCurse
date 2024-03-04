3d935964bd6ff3c9c2e4868346c9e96fa968d403
Antares <elgranpalicron@gmail.com>
2024-03-03 23:26:11 -0500

    Class 149

--- Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -41,7 +41,17 @@ void UAuraProjectileSpell::SpawnProjectile(const FVector& ProjectileTargetLocati
 
 			const UAbilitySystemComponent* SourceASC =  UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(GetAvatarActorFromActorInfo());
 
-			const FGameplayEffectSpecHandle SpecHandel = SourceASC->MakeOutgoingSpec(DamageEffectClass, GetAbilityLevel(),SourceASC->MakeEffectContext() );
+			FGameplayEffectContextHandle EffectContextHandle = SourceASC->MakeEffectContext();
+			EffectContextHandle.SetAbility(this);
+			EffectContextHandle.AddSourceObject(Projectile);
+			TArray<TWeakObjectPtr<AActor>> Actors;
+			Actors.Add(Projectile);
+			EffectContextHandle.AddActors(Actors);
+			FHitResult HitResult;
+			HitResult.Location =  ProjectileTargetLocation;
+			EffectContextHandle.AddHitResult(HitResult);
+			
+			const FGameplayEffectSpecHandle SpecHandel = SourceASC->MakeOutgoingSpec(DamageEffectClass, GetAbilityLevel(), EffectContextHandle);
 			FAuraGamePlayTags GamePlayTags = FAuraGamePlayTags::Get();
 		
 			UAbilitySystemBlueprintLibrary::AssignTagSetByCallerMagnitude(SpecHandel,GamePlayTags.Damage,Damage.GetValueAtLevel(GetAbilityLevel()));
