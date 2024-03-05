51f22acc6cdb42dcb613e6e6bd8bae496ee2b732
JsPalacios <jspalacios@teravisiongames.com>
2024-03-05 15:58:48 -0500

    mitad 154

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemGlobals.cpp
@@ -0,0 +1,11 @@
+// CAS Tutorial JsPalacios
+
+
+#include "AbilitySystem/AuraAbilitySystemGlobals.h"
+
+#include "AuraAbilityTypes.h"
+
+FGameplayEffectContext* UAuraAbilitySystemGlobals::AllocGameplayEffectContext() const
+{
+	return new FAuraGameplayEffectContext();
+}
