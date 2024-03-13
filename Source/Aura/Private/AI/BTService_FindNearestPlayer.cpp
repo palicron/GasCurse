ba8f184b4828b8c28f8f61b83a29a27c0405d24f
Antares <elgranpalicron@gmail.com>
2024-03-12 22:35:09 -0500

    class 165

---
+++ Source/Aura/Private/AI/BTService_FindNearestPlayer.cpp
@@ -0,0 +1,11 @@
+// CAS Tutorial JsPalacios
+
+
+#include "AI/BTService_FindNearestPlayer.h"
+
+void UBTService_FindNearestPlayer::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+	
+}
