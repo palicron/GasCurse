4e5c2c209e7ab33cf6001829a293c4b4583d9c9b
Antares <elgranpalicron@gmail.com>
2024-03-14 22:18:03 -0500

    class 166

--- Source/Aura/Private/AI/BTService_FindNearestPlayer.cpp
+++ Source/Aura/Private/AI/BTService_FindNearestPlayer.cpp
@@ -3,9 +3,17 @@
 
 #include "AI/BTService_FindNearestPlayer.h"
 
+#include "AIController.h"
+#include "Kismet/GameplayStatics.h"
+
 void UBTService_FindNearestPlayer::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
 {
 	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
 
-	
+	APawn* OwningPawn = AIOwner->GetPawn();
+
+	const FName TargetTag = OwningPawn->ActorHasTag(FName("Player"))? FName("Enemy") : FName("Player");
+
+	TArray<AActor*> ActorWhitTag;
+	UGameplayStatics::GetAllActorsWithTag(OwningPawn,TargetTag,ActorWhitTag);
 }
