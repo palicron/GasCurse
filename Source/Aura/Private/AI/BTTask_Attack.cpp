288bed264acd8751f71d944762b99dd194b6cbb2
Antares <elgranpalicron@gmail.com>
2024-03-15 23:41:39 -0500

    class 170

---
+++ Source/Aura/Private/AI/BTTask_Attack.cpp
@@ -0,0 +1,9 @@
+// CAS Tutorial JsPalacios
+
+
+#include "AI/BTTask_Attack.h"
+
+EBTNodeResult::Type UBTTask_Attack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	return Super::ExecuteTask(OwnerComp, NodeMemory);
+}
