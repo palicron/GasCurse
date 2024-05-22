11e8104cb43b87a6d82c8de3a6caaf4998516477
TERAVISION\spalacios <JsPalacios>
2024-05-22 08:47:06 -0500

    class 244

---
+++ Source/Aura/Private/AbilitySystem/AsyncTasks/WaitCooldownChange.cpp
@@ -0,0 +1,79 @@
+// CAS Tutorial JsPalacios
+
+
+#include "AbilitySystem/AsyncTasks/WaitCooldownChange.h"
+
+#include "AbilitySystemComponent.h"
+
+UWaitCooldownChange* UWaitCooldownChange::WaitForCooldownChange(UAbilitySystemComponent* AbilitySystemComponent,
+                                                                const FGameplayTag& InCooldownTag)
+{
+	UWaitCooldownChange* WaitCooldownChange = NewObject<UWaitCooldownChange>();
+	WaitCooldownChange->ASC = AbilitySystemComponent;
+	WaitCooldownChange->CooldownTag = InCooldownTag;
+
+	if(!IsValid(AbilitySystemComponent) || !InCooldownTag.IsValid())
+	{
+		WaitCooldownChange->EndTask();
+		return nullptr;
+	}
+	
+	AbilitySystemComponent->RegisterGameplayTagEvent(InCooldownTag,EGameplayTagEventType::NewOrRemoved).AddUObject(WaitCooldownChange,&UWaitCooldownChange::CooldownTagChanged);
+	AbilitySystemComponent->OnActiveGameplayEffectAddedDelegateToSelf.AddUObject(WaitCooldownChange,&UWaitCooldownChange::OnActiveEffectAdded);
+	return WaitCooldownChange;
+}
+
+void UWaitCooldownChange::EndTask()
+{
+	if(!IsValid(ASC))
+	{
+		return;
+	}
+	ASC->RegisterGameplayTagEvent(CooldownTag,EGameplayTagEventType::NewOrRemoved).RemoveAll(this);
+	ASC->OnActiveGameplayEffectAddedDelegateToSelf.RemoveAll(this);
+	
+	SetReadyToDestroy();
+	MarkAsGarbage();
+}
+
+void UWaitCooldownChange::CooldownTagChanged(const FGameplayTag InCooldownTag, int32 NewCount)
+{
+	if(NewCount == 0)
+	{
+		CoolDownEnd.Broadcast(0.f);
+	}
+}
+
+void UWaitCooldownChange::OnActiveEffectAdded(UAbilitySystemComponent* TargetAsc,const FGameplayEffectSpec& EffectApply, FActiveGameplayEffectHandle ActiveEffectHandle)
+{
+	if(!ASC)
+	{
+		return;
+	}
+	FGameplayTagContainer AssetTags;
+	EffectApply.GetAllAssetTags(AssetTags);
+	
+	FGameplayTagContainer GrantedTags;
+	EffectApply.GetAllGrantedTags(GrantedTags);
+
+	if (AssetTags.HasTagExact(CooldownTag) || GrantedTags.HasTagExact(CooldownTag))
+	{
+		FGameplayEffectQuery GameplayEffectQuery = FGameplayEffectQuery::MakeQuery_MatchAllOwningTags(CooldownTag.GetSingleTagContainer());
+		TArray<float> TimesRemaining =  ASC->GetActiveEffectsDuration(GameplayEffectQuery);
+		if(TimesRemaining.Num() > 0)
+		{
+			float HighestTime = TimesRemaining[0];
+			for(int32 i = 0; i<TimesRemaining.Num();i++)
+			{
+				if(TimesRemaining[i] > HighestTime )
+				{
+					HighestTime = TimesRemaining[i];
+				}
+					
+			}
+		
+			CoolDownStart.Broadcast(HighestTime);
+		}
+		
+	}
+}
---
+++ Source/Aura/Public/AbilitySystem/AsyncTasks/WaitCooldownChange.h
@@ -0,0 +1,48 @@
+// CAS Tutorial JsPalacios
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffect.h"
+#include "GameplayTagContainer.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "WaitCooldownChange.generated.h"
+
+
+class UAbilitySystemComponent;
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FCoolDownChangeSignature, float, TimeRemaining);
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UWaitCooldownChange : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(BlueprintAssignable)
+	FCoolDownChangeSignature CoolDownStart;
+
+	UPROPERTY(BlueprintAssignable)
+	FCoolDownChangeSignature CoolDownEnd;
+
+	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true"))
+	static UWaitCooldownChange* WaitForCooldownChange(UAbilitySystemComponent* AbilitySystemComponent, const FGameplayTag& InCooldownTag);
+
+	UFUNCTION(BlueprintCallable)
+	void EndTask();
+
+protected:
+
+	UPROPERTY()
+	TObjectPtr<UAbilitySystemComponent> ASC;
+
+	FGameplayTag CooldownTag;
+
+	UFUNCTION()
+	void CooldownTagChanged(const FGameplayTag InCooldownTag, int32 NewCount);
+
+	UFUNCTION()
+	void OnActiveEffectAdded(UAbilitySystemComponent* TargetAsc, const FGameplayEffectSpec& EffectApply,FActiveGameplayEffectHandle ActiveEffectHandle);
+};
