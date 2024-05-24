c8f94515643e9e5beb7dbf902638e561a90f9d1e
Antares <elgranpalicron@gmail.com>
2024-05-24 12:50:48 -0500

    class 249

---
+++ Source/Aura/Private/AbilitySystem/Data/LevelUpInfo.cpp
@@ -0,0 +1,30 @@
+// CAS Tutorial JsPalacios
+
+
+#include "AbilitySystem/Data/LevelUpInfo.h"
+
+int32 ULevelUpInfo::FindLevelForCp(int32 XP)
+{
+	int32 level = 1;
+	bool bSearching = true;
+
+	while(bSearching)
+	{
+		if(LevelUpInfos.Num() -1 <= level)
+		{
+			return level;
+		}
+
+		if(XP >= LevelUpInfos[level].LevelUpRequirement)
+		{
+			level++;
+		}
+		else
+		{
+			bSearching = false;
+		}
+	}
+
+	return level;
+	
+}
---
+++ Source/Aura/Public/AbilitySystem/Data/LevelUpInfo.h
@@ -0,0 +1,39 @@
+// CAS Tutorial JsPalacios
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "LevelUpInfo.generated.h"
+
+USTRUCT()
+struct FAuraLevelUpInfo
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditDefaultsOnly)
+	int32 LevelUpRequirement = 0;
+
+	UPROPERTY(EditDefaultsOnly)
+	int32 AttributePointAward = 1;
+
+	UPROPERTY(EditDefaultsOnly)
+	int32 SpellPointAward = 1;
+	
+};
+/**
+ * 
+ */
+UCLASS()
+class AURA_API ULevelUpInfo : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(EditDefaultsOnly)
+	TArray<FAuraLevelUpInfo> LevelUpInfos;
+
+	int32 FindLevelForCp(int32 XP);
+	
+};
