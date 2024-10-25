82ae7de0fe52e04b4d763b3dc7a61f542edc43e6
TERAVISION\spalacios <JsPalacios>
2024-10-25 09:51:23 -0500

    class 342

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -292,9 +292,16 @@ void UAuraAttributeSet::HandleIncomingXP(const FEffectProperties& Props)
 		const int32 NumLevelUps = NewLevel - CurrentLevel;
 		if(NumLevelUps > 0)
 		{
-		
-			const int32 AttributePointsReward = IPlayerInterface::Execute_GetAttributePointReward(Props.SourceCharacter,CurrentLevel);
-			const int32 SpellPointReward = IPlayerInterface::Execute_GetSpellPointsReward(Props.SourceCharacter,CurrentLevel);
+			
+			int32 AttributePointsReward = 0;
+			int32 SpellPointReward = 0;
+
+			for (int32 i = 0; i < NumLevelUps; i++)
+			{
+				SpellPointReward += IPlayerInterface::Execute_GetSpellPointsReward(Props.SourceCharacter, CurrentLevel + i);
+				AttributePointsReward += IPlayerInterface::Execute_GetAttributePointReward(Props.SourceCharacter, CurrentLevel + i);
+			}
+			
 			IPlayerInterface::Execute_AddToPlayerLevel(Props.SourceCharacter,NumLevelUps);
 			IPlayerInterface::Execute_LevelUp(Props.SourceCharacter);
 			IPlayerInterface::Execute_AddToAttributePoints(Props.SourceCharacter,AttributePointsReward);
