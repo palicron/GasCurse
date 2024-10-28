9c0f3d4c83461583faa1412369a3dcfb1ff0363a
TERAVISION\spalacios <JsPalacios>
2024-10-28 08:48:00 -0500

    class 345

---
+++ Source/Aura/Public/Actor/MagicCircle.h
@@ -0,0 +1,28 @@
+// CAS Tutorial JsPalacios
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MagicCircle.generated.h"
+
+class UDecalComponent;
+
+UCLASS()
+class AURA_API AMagicCircle : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	
+	AMagicCircle();
+
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UDecalComponent> MagicCircleDecal;
+};
